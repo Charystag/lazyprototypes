b1b5bd0c35c84be3053f8a6f222e5a9f56c30377
nsainton <nsainton@student.42.fr>
2023-08-05 10:11:31 +0200

    Added automatic header creation, need to integrate it with other part of header script

--- dynamic_header.c
+++ dynamic_header.c
@@ -6,7 +6,7 @@
 /*   By: nsainton <nsainton@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/04 17:00:35 by nsainton          #+#    #+#             */
-/*   Updated: 2023/08/04 17:21:16 by nsainton         ###   ########.fr       */
+/*   Updated: 2023/08/05 10:10:43 by nsainton         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,48 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <string.h>
+#include <sys/wait.h>
+#include <stdlib.h>
 # define SCRIPT_NAME "42dynamicheaderscript.sh"
 
-static int	write_script(void)
-{
-	int	script_fd;
-	int	err;
+extern char **environ;
 
-	if ((script_fd = open(SCRIPT_NAME, O_CREAT | O_WRONLY, 00700)) == -1)
+/*
+	PATH_SIZE is defined as 1024 in header_int.h
+	We allocate a large char array for the path in order to get sure
+	that even for directories that are nested deep into the files tree
+	the path won't overflow the path array
+*/
+static int	get_path(const char *name, char *path_array)
+{
+	if (*name == '/')
 	{
-		fputs("Can't open file : SCRIPT_NAME\n", stderr);
+		strcpy(path_array, name);
+		return (0);
+	}
+	if (getcwd(path_array, PATH_SIZE) == NULL)
+	{
+		perror("Can't retrieve current working directory");
+		return (1);
+	}
+	strcat(path_array, "/");
+	strcat(path_array, name);
+	return (0);
+}
+	
+static int	write_script(const char *script_path)
+{
+	int		script_fd;
+	int		err;
+
+	if (! access(script_path, F_OK) && remove(script_path) == -1)
+	{
+		fprintf(stderr, "Can't remove : %s\n", script_path);
+		return (1);
+	}
+	if ((script_fd = open(script_path, O_CREAT | O_WRONLY, 00700)) == -1)
+	{
+		fprintf(stderr, "Can't open file : %s\n", script_path);
 		return (1);
 	}
 	err = (write(script_fd, HEADER_SCRIPT, strlen(HEADER_SCRIPT)) < 1);
@@ -34,17 +66,51 @@ static int	write_script(void)
 	return (err);
 }
 
-int	main(int argc, char **argv, char **envp)
+static void	set_args(const char *args[3], const char *script_path, \
+const char *header_path)
+{
+	*args = script_path;
+	*(args + 1) = header_path;
+	*(args + 2) = NULL;
+}
+
+static int	dynamic_header(const char *header_path, \
+const char *script_name)
+{
+	char		script_path[PATH_SIZE];
+	const char	*args[3];
+	int			status;
+	int			pid;
+
+	if (environ == NULL)
+		return (1);
+	if (get_path(script_name, script_path) || write_script(script_path))
+		return (1);
+	pid = fork();
+	if (pid < 0)
+	{
+		fprintf(stderr, "Couldn't fork process\n");
+		return (1);
+	}
+	if (pid == 0)
+	{
+		set_args(args, script_path, header_path);
+		execve(script_path, (char **)args, environ);
+		fprintf(stderr, "Couldn't use execve\n");
+		exit(1);
+	}
+	wait(&status);
+	return (status);
+}
+
+int	main(int argc, char **argv)
 {
 	(void)argc;
 	(void)argv;
-	(void)envp;
-	if (! access(SCRIPT_NAME, F_OK) && remove(SCRIPT_NAME) == -1)
+	if (dynamic_header("/home/nosainto/Documents/t.h", SCRIPT_NAME))
 	{
-		fputs("Couldn't remove file : SCRIPT_NAME\n", stderr);
+		fprintf(stderr, "Using default header instead\n");
 		return (1);
 	}
-	if (write_script())
-		return (1);
 	return (0);
 }
