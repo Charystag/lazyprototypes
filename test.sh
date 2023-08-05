b5c3d6b590cea6bcbc0ffbaddc629ad3062f4b4b
nsainton <nsainton@student.42.fr>
2023-08-05 14:57:47 +0200

    Improved test file, needs testing

--- test.sh
+++ test.sh
@@ -1,4 +1,34 @@
 #!/bin/bash
-echo -n "Would you like to install stdheader.vim plugin ? [y/n]"
-read -n1 reply
-echo $reply
+plugin_dir="$HOME/.vim/plugin"
+plugin_name="stdheader.vim"
+plugin_path="$plugin_dir"/"$plugin_name"
+refused_install="/tmp/.no_plugin_install_42dynamicheader"
+
+install_script() {
+	if [ -f $refused_install ]
+	echo "No stdheader plugin found"
+	echo -n "Would you like to install stdheader.vim plugin ? [y/n]"
+	read -n1 reply
+	until [ "$reply" = [yYnN] ]
+	do
+		echo "Unrecognized character, try again"
+		echo -n "Would you like to install stdheader.vim plugin ? [y/n]"
+		read -n1 reply
+	done
+	case "$reply" in
+	[yY] )
+		echo "Script will be installed" ;;
+	[nN] )
+		cat <<-NO_INSTALL
+		No script will be installed
+		If you wish to change your choise you can either install it 
+yourself or remove the file $refused_install
+		NO_INSTALL
+		touch $refused_install
+		exit 5
+}
+
+if [! $plugin_path && ! -f $refused_install ]
+then
+	install_script
+fi
