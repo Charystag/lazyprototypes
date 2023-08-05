b87cb10572b617396ba8eeacf5bbbf0559bf508d
nsainton <nsainton@student.42.fr>
2023-08-05 17:12:46 +0200

    Added prompt to dynamic header script

--- includes/header42.h
+++ includes/header42.h
@@ -25,14 +25,14 @@
 	So don't get tempted to add it in your vimrc, because it could mess with
 	some over projects you're building after.
 */
-# define HEADER_SCRIPT "#!/bin/sh\n" \
+#define HEADER_SCRIPT	"#!/bin/sh\n" \
 						"plugin_dir=\"$HOME/.vim/plugin\"\n" \
 						"plugin_name=\"stdheader.vim\"\n" \
-						"plugin_path=\"$plugin_dir/$plugin_name\"\n" \
+						"plugin_path=\"$plugin_dir\"/\"$plugin_name\"\n" \
 						"refused_install=\"/tmp/.no_plugin_install_42dynamicheader\"\n" \
 						"filename=\"$1\"\n" \
-						"vim_path=\"$(whereis vim | "\
-						"awk -F ':' '{print $2}')\"\n\n" \
+						"vim_path=\"$(whereis vim | awk -F ':' '{print $2}')\"\n" \
+						"\n" \
 						"install_script() {\n" \
 						"\tif [ -f $refused_install ]\n" \
 						"\tthen\n" \
@@ -42,12 +42,13 @@
 						"\tfi\n" \
 						"\techo \"No stdheader plugin found\"\n" \
 						"\techo -n \"Would you like to install stdheader.vim plugin ? [y/n]\"\n" \
-						"\tread -n1 reply\n" \
-						"\tuntil [[ $reply = [yYnN] ]]\n" \
+						"\tread reply\n" \
+						"\tuntil [ \"$reply\" = \"y\" ] || [ \"$reply\" = \"n\" ] \\\n" \
+						"\t|| [ \"$reply\" = \"Y\" ] || [ \"$reply\" = \"N\" ]\n" \
 						"\tdo\n" \
 						"\t\techo \"Unrecognized character, try again\"\n" \
 						"\t\techo -n \"Would you like to install stdheader.vim plugin ? [y/n]\"\n" \
-						"\t\tread -n1 reply\n" \
+						"\t\tread reply\n" \
 						"\tdone\n" \
 						"\tcase \"$reply\" in\n" \
 						"\t[yY] )\n" \
@@ -61,17 +62,17 @@
 						"\t\ttouch $refused_install\n" \
 						"\t\texit 5 ;;\n" \
 						"\tesac\n" \
-						"}\n\n" \
+						"}\n" \
+						"\n" \
 						"if [ \"$vim_path\" = \"\" ]\n" \
 						"then\n" \
 						"\tcat <<-VIM\n" \
-						"\t\tvim command is not installed, you need vim to " \
-						"use the dynamic header\n" \
-						"\t\tyou can install vim by running : \\`sudo apt " \
-						"install vim\\`\n" \
+						"\t\tvim command is not installed, you need vim to use the dynamic header\n" \
+						"\t\tyou can install vim by running : \\`sudo apt install vim\\`\n" \
 						"\t\tVIM\n" \
 						"\texit 3\n" \
-						"fi\n\n" \
+						"fi\n" \
+						"\n" \
 						"if [ ! -f $plugin_path ]\n" \
 						"then\n" \
 						"\tinstall_script\n" \
@@ -80,9 +81,7 @@
 						"\tthen\n" \
 						"\t\texit 4\n" \
 						"\tfi\n" \
-						"\tcurl -fsSL https://raw.githubusercontent.com/" \
-						"42Paris/42header/master/plugin/stdheader.vim -o \"" \
-						"$plugin_path\"\n" \
+						"\tcurl -fsSL https://raw.githubusercontent.com/42Paris/42header/master/plugin/stdheader.vim -o \"$plugin_path\"\n" \
 						"\tif [ ! -f $plugin_path ]\n" \
 						"\tthen\n" \
 						"\t\techo \"Couldn't get plugin file\"\n" \
@@ -93,18 +92,18 @@
 						"\t\t\tDon't forget to add lines :\n" \
 						"\t\t\tlet g:user42 = your_username\n" \
 						"\t\t\tlet g:mail42 = your_email\n" \
-						"\t\t\tto your $HOME/.vimrc file if not already done" \
-						" or to create one \\\n" \
+						"\t\t\tto your $HOME/.vimrc file if not already done or to create one \\\n" \
 						"with those two lines if you don't use vim\n" \
 						"\t\t\tINSTRUCTIONS\n" \
 						"\tfi\n" \
-						"fi\n\n" \
+						"fi\n" \
+						"\n" \
 						"if [ \"$filename\" = \"\" ]\n" \
 						"then\n" \
 						"\techo \"No filename provided\"\n" \
 						"\texit 2\n" \
-						"fi\n\n" \
-						"vim \"+set binary\" \"+set noeol\" \"+Stdheader\" " \
-						"\"+wq\" $filename\n" \
+						"fi\n" \
+						"\n" \
+						"vim \"+set binary\" \"+set noeol\" \"+Stdheader\" \"+wq\" $filename\n" \
 						"exit 0\n"
 #endif
